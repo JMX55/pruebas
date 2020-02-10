52bb3bd2fa84498819e7b6f748d84302114a0955
JMX55 <jmx55.jl@gmail.com>
2020-02-10 15:16:06 +0100

    Ejercicos curso python

--- comer_helado.py
+++ comer_helado.py
@@ -7,19 +7,18 @@ elif apetece_helado_input == "No":
     apetece_helado = False
 else:
     print("Te he dicho que me digas si o no, como no te entiendo cuento como que no")
-    apete_helado = False
+    apetece_helado = False
 
 tiene_dinero_input = input("Tienes dinero para un helado? (Si / No)")
 esta_el_heladero_input = input("Esta el heladero? (Si / No)")
 esta_tu_tia_input = input("Estás con tu tia? (Si/ No)")
 
-apetece_helado = apetece_helado_input == "Si"
-tiene_dinero = tiene_dinero_input == "Si"
-esta_tu_tia = esta_tu_tia_input == "Si"
-puedes_permitirtelo = tiene_dinero or esta_tu_tia
-esta_el_heladero = esta_el_heladero_input == "Si"
+apetece_helado = apetece_helado_input
+tiene_dinero = tiene_dinero_input
+esta_tu_tia = esta_tu_tia_input
+esta_el_heladero = esta_el_heladero_input
 
-if apetece_helado == "Si" and puedes_permitirtelo == "Si" and esta_el_heladero:
+if apetece_helado == "Si"and esta_el_heladero == "Si" and (tiene_dinero == "Si" or esta_tu_tia == "Si"):
     print ("Comete un helado")
 else:
     print ("Pues nada")
